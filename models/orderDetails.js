75d28df13e056aff55164be9f294d42d78c50a84
harismehar5 <harismehar3@gmail.com>
2022-11-10 16:05:40 +0500

    Validation

--- models/orderDetails.js
+++ models/orderDetails.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const OrderDetails = new mongoose.Schema({
   quantity: {
     type: Number,
-    required: true,
+    required: [true, 'Please Enter Product Quantity'],
     default: 0,
   },
   amount: {
     type: Number,
-    required: true,
+    required: [true, 'Please Enter Product Amount'],
     default: 0,
   },
   product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
